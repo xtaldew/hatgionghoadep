9017325e3d0b08979a93b63447152524d6c81705
Bui Van Dieu <buivandieu91@gmail.com>
2016-07-21 22:49:28 +0700

    initial code for site

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+
+// Edited by ThietkewebX.net
+
+// Cấu hình kết nối dữ liệu
+$hostname     = "localhost"; // Tên Host
+$username     = "root"; // Tên user đăng nhập
+$password     = ""; // Mật khẩu đăng nhập
+$databasename = "mysql"; // Tên cơ sở dữ liệu, nếu không cấu hình đúng, sẽ ra trang trắng.
+
+// Cấu hình hệ thống
+$visitorTimeout = 900; //=15 * 60
+$MAXPAGE = 10;
+$multiLanguage = 1;//0 : single  ;  1 : multi
+
+// Cấu hình ngôn ngữ.
+$arrLanguage = array(
+	array('vn','Việt Nam'),
+	array('en','English')
+);
+
+?>
+
